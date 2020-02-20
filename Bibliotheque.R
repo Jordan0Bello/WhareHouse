c87affed5ab6ce5418a45ff73e7ee97f5afe6467
Jordan0Bello <38489147+Jordan0Bello@users.noreply.github.com>
2020-02-20 10:17:07 +0100

    Add files via upload

---
+++ Bibliotheque.R
@@ -0,0 +1,92 @@
+# Biblioth?que Municipale
+if (!require(shiny))
+  install.packages("shiny")
+
+if (!require(leaflet))
+install.packages("leaflet")
+
+if (!require(shinydashboard))
+install.packages("shinydashboard")
+
+if (!require(FactoMineR))
+install.packages("FactoMineR")
+
+if (!require(shinydashboard))
+install.packages("shinydashboard")
+
+if (!require(data.table))
+  install.packages("data.table")
+
+if (!require(raster))
+install.packages("raster")
+
+if (!require(sp))
+install.packages("sp")
+
+if (!require(rgdal))
+install.packages("rgdal")
+
+if (!require(cartography))
+install.packages("cartography")
+
+if (!require(SpatialPosition))
+install.packages("SpatialPosition")
+
+if (!require(tidyverse))
+install.packages("tidyverse")
+
+if (!require(knitr))
+install.packages("knitr")
+
+if (!require(rmarkdown))
+install.packages("rmarkdown")
+
+if (!require(ggpubr))
+install.packages("ggpubr")
+
+if (!require(flexdashboard))
+install.packages("flexdashboard")
+
+if (!require(xlsx))
+  install.packages("xlsx")
+
+if (!require(boot))
+  install.packages("boot")
+
+if (!require(dplyr))
+  install.packages("dplyr")
+
+if (!require(doBy))
+  install.packages("doBy")
+
+if (!require(tidyr))
+  install.packages("tidyr")
+
+if (!require(plotly))
+  install.packages("plotly")
+
+if (!require(quantmod))
+  install.packages("quantmod")
+
+# library(shiny)
+# library(leaflet)
+# library(shinydashboard)
+# library(FactoMineR)
+# library(data.table)
+# library(raster)
+# library(sp)
+# library(sf)
+# library(rgdal)
+# library(cartography)
+# library(SpatialPosition)
+# library(xlsx)
+# library(boot)
+# library("dplyr")
+# library(doBy)
+# library(tidyr)
+# library(plotly)
+# library(quantmod)
+
+
+
+
